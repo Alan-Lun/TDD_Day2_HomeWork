22f61119863f089515da930f96af080ea26cc7a4
黃振倫 <s52076663135@gmail.com>
2016-03-18 22:34:02 +0800

    第四個測試全部OK，回傳成功指定的320

--- ShoppingTests/ShoppingCartTests.cs
+++ ShoppingTests/ShoppingCartTests.cs
@@ -70,5 +70,27 @@ namespace Shopping.Tests
             //Assert.Fail();
         }
 
+        [TestMethod()]
+        public void CheckOutTest_購買第一集一本和第二集一本和第三集一本和第四集一本_價格應為320()
+        {
+            //arrange 初始化
+            List<Book> _books = new List<Book>();
+            _books.Add(new Book { BookName = "哈利波特第一集", Num = 1, Price = 100, CheckOut = false });
+            _books.Add(new Book { BookName = "哈利波特第二集", Num = 1, Price = 100, CheckOut = false });
+            _books.Add(new Book { BookName = "哈利波特第三集", Num = 1, Price = 100, CheckOut = false });
+            _books.Add(new Book { BookName = "哈利波特第四集", Num = 1, Price = 100, CheckOut = false });
+
+            var target = new ShoppingCart();
+
+            var expected = 320;
+
+            //act 測試目標行為
+            var actual = target.CheckOut(_books);
+
+            //assert 驗證目標行為
+            Assert.AreEqual(expected, actual);
+            //Assert.Fail();
+        }
+
     }
 }
