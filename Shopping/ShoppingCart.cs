07dfe957833efed72910cff2af16a857c5dc8209
黃振倫 <s52076663135@gmail.com>
2016-03-18 20:55:13 +0800

    初始建立

---
+++ Shopping/ShoppingCart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shopping
+{
+    public class ShoppingCart
+    {
+        public int CheckOut()
+        {
+            return 100;
+        }
+
+
+
+    }
+}
---
+++ ShoppingTests/ShoppingCartTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shopping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shopping.Tests
+{
+    [TestClass()]
+    public class ShoppingCartTests
+    {
+        [TestMethod()]
+        public void CheckOutTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
